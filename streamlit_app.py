2c9012248cbfccc44e5df0f0fd41bc08c1eb45de
tomb43508 <73494360+tomb43508@users.noreply.github.com>
2023-02-10 14:18:36 -0600

    Update streamlit_app.py
    Added in pandas to read in a fruit macros listing file from S3 buckets

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.text('🧋🥬Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔🥚Hard-Boiled Free-Range Eggs')
 streamlit.text('🥑🍞Avacado Toast')
 streamlit.header('🍌🍓Build Your Own Fruit Smoothie🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
