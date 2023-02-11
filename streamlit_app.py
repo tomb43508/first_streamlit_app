d3b1229913d3a207e28ee0016a49c85705d5c89b
tomb43508 <73494360+tomb43508@users.noreply.github.com>
2023-02-10 19:57:01 -0600

    Update streamlit_app.py
    easier to read

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Maek it easier to read 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display it in a nicer format
+streamlit.dataframe(fruityvice_normalized)
