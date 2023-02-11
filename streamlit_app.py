9c98ccbb450ac0d6156c73b54ba2541fbf2fb3a1
tomb43508 <73494360+tomb43508@users.noreply.github.com>
2023-02-11 17:53:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
-# Maek it easier to read 
+# Make it easier to read 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display it in a nicer format
 streamlit.dataframe(fruityvice_normalized)
@@ -39,5 +39,5 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit list contains:")
-streamlit.text(my_data_row)
+streamlit.header("The fruit list contains:")
+streamlit.dataframe(my_data_row)
